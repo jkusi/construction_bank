1e453c09eb14bc0d38b8333d64a408cde518f837
jkusi <jeremiahhero70@gmail.com>
2023-07-16 07:32:38 +0000

    modified:   src/main/java/com/example/mybank/LoginServlet.java 	modified:   target/classes/com/example/mybank/LoginServlet.class

--- src/main/java/com/example/mybank/LoginServlet.java
+++ src/main/java/com/example/mybank/LoginServlet.java
@@ -10,7 +10,6 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 @WebServlet("/login")
@@ -24,9 +23,6 @@ public class LoginServlet extends HttpServlet {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-        // Validate login credentials and interact with the database
-        boolean isValid = validateCredentials(username, password);
-
         // Store the credentials in the database
         try {
             insertUser(username, password);
@@ -37,31 +33,7 @@ public class LoginServlet extends HttpServlet {
         }
 
         // Redirect after a successful login
-        if (isValid) {
-            response.sendRedirect(request.getContextPath() + "/dashboard.html");
-        } else {
-            response.sendRedirect(request.getContextPath() + "/dashboard.html");
-        }
-    }
-
-    private boolean validateCredentials(String username, String password) {
-        try (Connection connection = createConnection();
-             PreparedStatement statement = connection.prepareStatement("SELECT COUNT(*) FROM users WHERE username = ? AND password = ?")) {
-
-            statement.setString(1, username);
-            statement.setString(2, password);
-
-            try (ResultSet resultSet = statement.executeQuery()) {
-                // Check if there is a matching user
-                resultSet.next();
-                int count = resultSet.getInt(1);
-                return count > 0;
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            // Handle the exception and return appropriate response or error message
-        }
-        return false;
+        response.sendRedirect(request.getContextPath() + "/dashboard.html");
     }
 
     private void insertUser(String username, String password) throws SQLException {
@@ -76,7 +48,7 @@ public class LoginServlet extends HttpServlet {
 
     private Connection createConnection() throws SQLException {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             throw new SQLException("Failed to load MySQL JDBC driver.");
         }
