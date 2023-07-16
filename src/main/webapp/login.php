7c575248693ee316ed892d31d5704ac1c65c6bed
jkusi <jeremiahhero70@gmail.com>
2023-07-16 02:20:23 +0000

    modified:   src/main/webapp/login.php

--- src/main/webapp/login.php
+++ src/main/webapp/login.php
@@ -1,29 +1,28 @@
 <?php
 // Check if the user is logged in
-$username = $_POST["username"];
-$password = $_POST["password"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
 
-$hostname = "192.168.50.23";
-$username = "king";
-$password = "Kusi@123";
-$database = "bank";
+    $hostname = "192.168.50.23";
+    $db_username = "king";
+    $db_password = "Kusi@123";
+    $database = "bank";
 
-$conn = mysqli_connect($hostname, $username, $password, $database);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    $conn = mysqli_connect($hostname, $db_username, $db_password, $database);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Store the credentials in the database
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ss", $username, $hashedPassword);
+    $stmt->execute();
+
+    // Redirect the user to the dashboard page
+    header("Location: dashboard.html");
+    exit();
 }
-
-$sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  // The user is logged in, redirect them to the dashboard page
-  header("Location: dashboard.html");
-} else {
-  // The user is not logged in, show an error message
-  echo "Invalid username or password.";
-}
-
-$conn->close();
 ?>
-
