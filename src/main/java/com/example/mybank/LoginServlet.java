c50ff6456360d9f7155ef3d281d64f0076170b04
jkusi <73686281+jkusi@users.noreply.github.com>
2023-07-30 21:13:18 -0700

    updated

--- src/main/java/com/example/mybank/LoginServlet.java
+++ src/main/java/com/example/mybank/LoginServlet.java
@@ -10,56 +10,57 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.ResultSet;
 
-@WebServlet("/login")
+@WebServlet(urlPatterns = { "/login" })
 public class LoginServlet extends HttpServlet {
-    private static final String DB_URL = "jdbc:mysql://192.168.50.23:3306/bank";
-    private static final String DB_USER = "king";
+
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/dev";
+    private static final String DB_USERNAME = "dev";
     private static final String DB_PASSWORD = "Kusi@123";
 
     @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Show the login form
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Login</h1>");
+        out.println("<form action=\"/login\" method=\"post\">");
+        out.println("<input type=\"text\" name=\"username\" placeholder=\"Username\">");
+        out.println("<input type=\"password\" name=\"password\" placeholder=\"Password\">");
+        out.println("<input type=\"submit\" value=\"Login\">");
+        out.println("</form>");
+        out.close();
+    }
+
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Get the username and password from the form
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-        // Store the credentials in the database
-        try {
-            insertUser(username, password);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            sendErrorResponse(response, "Error inserting user into the database.");
-            return;
-        }
-
-        // Redirect after a successful login
-        response.sendRedirect(request.getContextPath() + "/dashboard.html");
-    }
-
-    private void insertUser(String username, String password) throws SQLException {
-        try (Connection connection = createConnection();
-             PreparedStatement statement = connection.prepareStatement("INSERT INTO users (username, password) VALUES (?, ?)")) {
-
+        // Check the username and password against the database
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
+            String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
             statement.setString(1, username);
             statement.setString(2, password);
-            statement.executeUpdate();
-        }
-    }
 
-    private Connection createConnection() throws SQLException {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new SQLException("Failed to load MySQL JDBC driver.");
-        }
-        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-    }
+            ResultSet resultSet = statement.executeQuery();
 
-    private void sendErrorResponse(HttpServletResponse response, String errorMessage) throws IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<h1>Error</h1>");
-        out.println("<p>" + errorMessage + "</p>");
-        out.close();
+            if (resultSet.next()) {
+                // The username and password are correct, so redirect to the dashboard page
+                response.sendRedirect(request.getContextPath() + "/dashboard.html");
+            } else {
+                // The username and password are incorrect, so display an error message
+                response.setContentType("text/html");
+                PrintWriter out = response.getWriter();
+                out.println("<h1>Login failed</h1>");
+                out.println("The username and password are incorrect. Please try again.");
+                out.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
